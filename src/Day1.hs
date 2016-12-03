3f6895501a4d59c39a00c1506e482bc20364e7da
Steve Purcell <steve@sanityinc.com>
2016-12-04 08:37:09 +1300

    Day 1b

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,49 +1,47 @@
+{-# LANGUAGE TupleSections #-}
 module Day1
   ( day1
-  , runInstructions)
+  )
 where
 
+import           Data.List          (groupBy)
+import           Data.Maybe         (listToMaybe)
+import           Data.Set           (Set)
+import qualified Data.Set           as S
 import           Text.Parsec        hiding (State)
 import           Text.Parsec.String
 
 
 data TurnDirection = L | R
 
-data Instruction = Instruction TurnDirection Integer
-
+data Instruction = Instruction { iDir    :: TurnDirection
+                               , iBlocks :: Blocks}
 
 data Direction = N | E | S | W
+  deriving Show
 
-type Blocks = Integer
 
-data Position = Position { pFacing :: Direction
-                         , pNetN   :: Blocks
-                         , pNetE   :: Blocks }
+type Blocks = Int
+
+data Position = Position { pNetN :: Blocks
+                         , pNetE :: Blocks }
+                deriving (Eq, Ord, Show)
 
 shortestDistance :: Position -> Blocks
-shortestDistance (Position _ n e) = abs n + abs e
+shortestDistance (Position n e) = abs n + abs e
 
-initialPosition = Position N 0 0
+move :: Position -> Direction -> Position
+move p@(Position n e) N = p { pNetN = n + 1 }
+move p@(Position n e) S = p { pNetN = n - 1 }
+move p@(Position n e) E = p { pNetE = e + 1 }
+move p@(Position n e) W = p { pNetE = e - 1 }
 
-
-forward :: Position -> Blocks -> Position
-forward p@(Position N n e) b = p { pNetN = n + b }
-forward p@(Position S n e) b = p { pNetN = n - b }
-forward p@(Position E n e) b = p { pNetE = e + b }
-forward p@(Position W n e) b = p { pNetE = e - b }
-
-rotate :: Position -> TurnDirection -> Position
-rotate p td = p { pFacing = rotated (pFacing p) td }
-  where
-    rotated N L = W
-    rotated E L = N
-    rotated S L = E
-    rotated W L = S
-    rotated d R = rotated (rotated (rotated d L) L) L
-
-move :: Position -> Instruction -> Position
-move p (Instruction td dist) = forward (rotate p td) dist
-
+rotate :: Direction -> TurnDirection -> Direction
+rotate N L = W
+rotate E L = N
+rotate S L = E
+rotate W L = S
+rotate d R = rotate (rotate (rotate d L) L) L
 
 
 parseInstruction :: Parser Instruction
@@ -55,12 +53,30 @@ parseInstruction = Instruction <$> parseDirection <*> parseNumber
 parseFile :: String -> IO (Either ParseError [Instruction])
 parseFile = parseFromFile (sepBy parseInstruction (string ", ") <* newline <* eof)
 
-runInstructions :: [Instruction] -> Blocks
-runInstructions instrs = shortestDistance $ foldl move initialPosition instrs
+runInstructions :: [Instruction] -> [Position]
+runInstructions instrs = scanl move (Position 0 0) moves
+  where
+    moves = concatMap expand (zip directions distances)
+    expand (dir, dist) = replicate dist dir
+    directions = tail $ scanl rotate N (iDir <$> instrs)
+    distances = iBlocks <$> instrs
+
+firstDuplicate :: Ord a => [a] -> Maybe a
+firstDuplicate = go S.empty
+  where go _ [] = Nothing
+        go seen (x:xs) = if x `S.member` seen then Just x
+                         else go (S.insert x seen) xs
+
+loadInput :: IO [Instruction]
+loadInput = do
+  result <- parseFile "input/1.txt"
+  case result of
+    Right instrs -> return instrs
+    Left e -> error (show e)
 
 day1 :: IO ()
 day1 = do
-  result <- parseFile "input/1.txt"
-  case result of
-    Right instrs -> print $ runInstructions instrs
-    Left e -> error (show e)
+  instrs <- loadInput
+  let positions = runInstructions instrs
+  print $ shortestDistance $ last positions
+  print $ shortestDistance <$> firstDuplicate positions
