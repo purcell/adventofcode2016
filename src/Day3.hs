ba57f4b798c21521e6fd00b9896e79b3a6c2f914
Steve Purcell <steve@sanityinc.com>
2016-12-04 17:45:16 +1300

    Day 3b

--- src/Day3.hs
+++ src/Day3.hs
@@ -5,7 +5,7 @@ where
 
 import           Text.Parsec        hiding (State)
 import           Text.Parsec.String
-
+import Data.List (transpose, splitAt)
 
 
 data Triangle = Triangle Int Int Int
@@ -17,20 +17,38 @@ possible (Triangle a b c) = all valid $ zip3 sides (drop 1 $ cycle sides) (drop 
     valid (s1, s2, s3) = s1 + s2 > s3
 
 
+spaceNumber :: Parser Int
+spaceNumber = spaces *> parseInt
+  where parseInt = read <$> many1 digit
+
 parseTriangle :: Parser Triangle
 parseTriangle = Triangle <$> spaceNumber <*> spaceNumber <*> spaceNumber
-  where
-    spaceNumber = spaces *> parseInt
-    parseInt = read <$> many1 digit
 
-loadInput :: IO [Triangle]
-loadInput = do
+loadInputByRows :: IO [Triangle]
+loadInputByRows = do
   result <- parseFromFile (many1 (parseTriangle <* newline) <* eof) "input/3.txt"
   case result of
     Right triangles -> return triangles
     Left e -> error (show e)
 
+groupsOf :: Int -> [a] -> [[a]]
+groupsOf = go []
+  where
+    go acc _ [] = acc
+    go acc n xs = go (before : acc) n after
+      where (before, after) = splitAt n xs
+
+loadInputByCols :: IO [Triangle]
+loadInputByCols = do
+  result <- parseFromFile (many1 (count 3 spaceNumber <* newline) <* eof) "input/3.txt"
+  case result of
+    Right rows -> return $ toTriangle <$> groupsOf 3 (concat (transpose rows))
+      where toTriangle [a, b, c] = Triangle a b c
+    Left e -> error (show e)
+
 day3 :: IO ()
 day3 = do
-  triangles <- loadInput
-  print $ length (filter possible triangles)
+  rowTriangles <- loadInputByRows
+  print $ length (filter possible rowTriangles)
+  colTriangles <- loadInputByCols
+  print $ length (filter possible colTriangles)
