8f1d70a8ee6bfa621626da12c2f15fb9ad218942
Steve Purcell <steve@sanityinc.com>
2016-12-05 21:22:54 +1300

    Reuse computed hashes between 5a and 5b

--- src/Day5.hs
+++ src/Day5.hs
@@ -1,7 +1,6 @@
 module Day5
   ( day5
   )
-
 where
 
 import Data.Digest.Pure.MD5 as MD5
@@ -22,21 +21,22 @@ interesting s = "00000" == take 5 s
 interestingHashes :: String -> [String]
 interestingHashes seed = filter interesting (hashes seed)
 
-solutionA :: String -> String
-solutionA seed = take 8 $ (!! 5) <$> interestingHashes seed
+solutionA :: [String] -> String
+solutionA hs = take 8 $ (!! 5) <$> hs
 
-solutionB :: String -> String
-solutionB seed =
+solutionB :: [String] -> String
+solutionB hs =
   M.elems $ head $ dropWhile incomplete $ scanl addPair M.empty pairs
   where
     addPair m (p, c) = if p `M.member` m then m else M.insert p c m
     incomplete m = M.keys m /= "01234567"
-    pairs = catMaybes $ (posAndChar . drop 5) <$> interestingHashes seed
+    pairs = catMaybes $ (posAndChar . drop 5) <$> hs
     posAndChar (p:c:_) | p >= '0' && p < '8' = Just (p, c)
     posAndChar _ = Nothing
 
 
 day5 :: IO ()
 day5 = do
-  putStrLn $ solutionA "ojvtpuvg"
-  putStrLn $ solutionB "ojvtpuvg"
+  let hs = interestingHashes "ojvtpuvg"
+  putStrLn $ solutionA hs
+  putStrLn $ solutionB hs
