fc6f33279925b1a567ec7de3083a30784432622f
Steve Purcell <steve@sanityinc.com>
2016-12-05 19:50:08 +1300

    Day 5b

--- src/Day5.hs
+++ src/Day5.hs
@@ -6,21 +6,37 @@ where
 
 import Data.Digest.Pure.MD5 as MD5
 import Data.String
+import Data.Maybe (catMaybes)
+import qualified Data.Map as M
 
 
 hashes :: String -> [String]
-hashes seed = hexMD5 . (seed ++) . show <$> [0..]
+hashes seed = hexMD5 . (seed ++) . show <$> [(0::Integer)..]
 
-
+hexMD5 :: String -> String
 hexMD5 = show . md5 . fromString
-
 
 interesting :: String -> Bool
 interesting s = "00000" == take 5 s
 
+interestingHashes :: String -> [String]
+interestingHashes seed = filter interesting (hashes seed)
 
-solution seed = take 8 $ (!! 5) <$> filter interesting (hashes seed)
+solutionA :: String -> String
+solutionA seed = take 8 $ (!! 5) <$> interestingHashes seed
+
+solutionB :: String -> String
+solutionB seed =
+  M.elems $ head $ dropWhile incomplete $ scanl addPair M.empty pairs
+  where
+    addPair m (p, c) = if p `M.member` m then m else M.insert p c m
+    incomplete m = M.keys m /= "01234567"
+    pairs = catMaybes $ (posAndChar . drop 5) <$> interestingHashes seed
+    posAndChar (p:c:_) | p >= '0' && p < '8' = Just (p, c)
+    posAndChar _ = Nothing
 
 
 day5 :: IO ()
-day5 = putStrLn $ solution "ojvtpuvg"
+day5 = do
+  putStrLn $ solutionA "ojvtpuvg"
+  putStrLn $ solutionB "ojvtpuvg"
