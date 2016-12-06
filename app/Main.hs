eb8d9253e42f0ca6354af4c74488b56363c1fc70
Steve Purcell <steve@sanityinc.com>
2016-12-07 10:07:59 +1300

    Move .dir-locals.el to top level and reformat Main.hs correspondingly

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,20 @@
 module Main where
 
-import System.Environment (getArgs)
-import           Lib
 import Control.Monad (forM_)
-
+import Lib
+import System.Environment (getArgs)
 
 day :: String -> IO ()
-day n = runDay $ case n of
-                   "1" -> day1
-                   "2" -> day2
-                   "3" -> day3
-                   "4" -> day4
-                   "5" -> day5
-                   "6" -> day6
-                   _ -> error $ "No such day " ++ show n
+day n =
+  runDay $
+  case n of
+    "1" -> day1
+    "2" -> day2
+    "3" -> day3
+    "4" -> day4
+    "5" -> day5
+    "6" -> day6
+    _ -> error $ "No such day " ++ show n
   where
     runDay action = banner >> action
     banner = putStrLn $ "==== DAY " ++ n
