28e2662f74a35f97a7e3e2d619b6ab8b788c267b
Steve Purcell <steve@sanityinc.com>
2016-12-07 21:32:38 +1300

    Day 7b

--- src/Day7.hs
+++ src/Day7.hs
@@ -6,36 +6,57 @@ module Day7
 
 import Text.Parsec hiding (State)
 import Text.Parsec.String
+import Data.List (partition, intersect)
 
-data Part
-  = Plain
-  | Bracketed
+data Net
+  = Super
+  | Hyper
 
-data IPAddress =
-  IPAddress [(Part, String)]
+newtype IPAddress =
+  IPAddress [(Net, String)]
+
+superHyper :: IPAddress -> ([String], [String])
+superHyper (IPAddress parts) = (snd <$> super, snd <$> hyper)
+  where
+    (super, hyper) = partition supernet parts
+    supernet (Super, _) = True
+    supernet _ = False
 
 abba :: String -> Bool
 abba (a:b:c:d:_)
   | a /= b && a == d && b == c = True
-abba (x:xs) = abba xs
+abba (_:xs) = abba xs
 abba _ = False
 
 canTLS :: IPAddress -> Bool
-canTLS (IPAddress parts) =
-  any abba (snd <$> filter plain parts) &&
-  not (any abba (snd <$> filter bracketed parts))
+canTLS a = any abba super && not (any abba hyper)
   where
-    plain (Plain, _) = True
-    plain _ = False
-    bracketed (Bracketed, _) = True
-    bracketed _ = False
+    (super, hyper) = superHyper a
+
+canSSL :: IPAddress -> Bool
+canSSL a =
+  not . null $ (invertABA <$> concatMap abas super) `intersect` concatMap abas hyper
+  where
+    (super, hyper) = superHyper a
+
+invertABA :: String -> String
+invertABA (a:b:_) = [b, a, b]
+invertABA _ = undefined
+
+abas :: String -> [String]
+abas = go []
+  where
+    go acc s@(a:b:c:_)
+      | a /= b && a == c = go ([a, b, c] : acc) (drop 1 s)
+    go acc (_:xs) = go acc xs
+    go acc _ = acc
 
 parseIPAddress :: Parser IPAddress
 parseIPAddress =
   IPAddress <$>
   many1
-    (((Plain, ) <$> many1 letter) <|>
-     ((Bracketed, ) <$> (string "[" *> many1 letter <* string "]")))
+    (((Super, ) <$> many1 letter) <|>
+     ((Hyper, ) <$> (string "[" *> many1 letter <* string "]")))
 
 loadInput :: IO [IPAddress]
 loadInput = do
@@ -49,3 +70,4 @@ day7 :: IO ()
 day7 = do
   input <- loadInput
   print $ length $ filter canTLS input
+  print $ length $ filter canSSL input
