127339194c1e6d9d3eea40cf63874b919be0a18f
Steve Purcell <steve@sanityinc.com>
2016-12-09 19:22:08 +1300

    Day 9b

--- src/Day9.hs
+++ src/Day9.hs
@@ -8,20 +8,28 @@ import Text.Parsec.String
 data Block
   = Repeat Int
            String
+           [Block]
   | Plain String
+  deriving (Show)
 
-expandBlock :: Block -> String
-expandBlock (Repeat n s) = concat $ replicate n s
-expandBlock (Plain s) = s
+blockLenA :: Block -> Int
+blockLenA (Repeat n s _) = n * length s
+blockLenA (Plain s) = length s
+
+blockLenB :: Block -> Int
+blockLenB (Repeat n _ bs) = n * sum (blockLenB <$> bs)
+blockLenB (Plain s) = length s
 
 parseBlock :: Parser Block
-parseBlock = (try parseRepeat) <|> (Plain <$> many1 letter)
+parseBlock = try parseRepeat <|> (Plain <$> many1 letter)
   where
     parseRepeat = do
       len <- char '(' *> number <* char 'x'
       mult <- number <* char ')'
       rest <- count len anyChar
-      pure $ Repeat mult rest
+      case runP (many parseBlock <* eof) () "block" rest of
+        Right subBlocks -> pure $ Repeat mult rest subBlocks
+        Left err -> fail (show err)
     number = read <$> many1 digit
 
 loadInput :: IO [Block]
@@ -34,5 +42,5 @@ loadInput = do
 day9 :: IO ()
 day9 = do
   input <- loadInput
-  let result = sum (length . expandBlock <$> input)
-  print result
+  print $ sum (blockLenA <$> input)
+  print $ sum (blockLenB <$> input)
