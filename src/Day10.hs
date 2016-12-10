a12984a24968ca201f964c4fca57341f38a4885b
Steve Purcell <steve@sanityinc.com>
2016-12-10 23:29:36 +1300

    Day 10b: more newtype action

--- src/Day10.hs
+++ src/Day10.hs
@@ -10,39 +10,44 @@ import Control.Monad.Writer
 import Data.List (sort)
 import Data.Maybe (fromMaybe)
 
-newtype BotNo = BotNo
-  { getBotNo :: Int
+newtype BotNo =
+  BotNo Int
+  deriving (Eq, Ord, Show)
+
+newtype OutputNo = OutputNo
+  { getOutputNo :: Int
   } deriving (Eq, Ord, Show)
 
 data Receiver
   = BotReceiver BotNo
-  | OutputReceiver Int
+  | OutputReceiver OutputNo
 
 data Instr
-  = Pass { bName :: BotNo
-         , bLow :: Receiver
-         , bHigh :: Receiver}
-  | Input { iVal :: Int
-          , iBot :: BotNo}
+  = PassLowHigh BotNo
+                Receiver
+                Receiver
+  | Input Int
+          BotNo
 
 parseInstr :: Parser Instr
 parseInstr =
   (Input <$> (string "value " *> number) <*> (string " goes to bot " *> botno)) <|>
-  (Pass <$> (string "bot " *> botno) <*> (string " gives low to " *> receiver) <*>
+  (PassLowHigh <$> (string "bot " *> botno) <*>
+   (string " gives low to " *> receiver) <*>
    (string " and high to " *> receiver))
   where
     botno = BotNo <$> number
     number = read <$> many1 digit
     receiver =
       (BotReceiver <$> (string "bot " *> botno)) <|>
-      (OutputReceiver <$> (string "output " *> number))
+      (OutputReceiver . OutputNo <$> (string "output " *> number))
 
-type BotState = WriterT [(Int, Int)] (State (Map BotNo [Int]))
+type BotState = WriterT [(OutputNo, Int)] (State (Map BotNo [Int]))
 
 runInstrs :: [Instr] -> BotState ()
 runInstrs [] = return ()
 runInstrs (Input v b:xs) = putVal b v >> runInstrs xs
-runInstrs (p@(Pass b lo hi):xs) = do
+runInstrs (p@(PassLowHigh b lo hi):xs) = do
   vals <- getVal b
   case sort vals of
     [v1, v2] -> do
@@ -63,16 +68,18 @@ putVal b i = do
   vals <- getVal b
   modify (M.insert b (i : vals))
 
+partA :: [Instr] -> [(BotNo, [Int])]
 partA =
   filter (\(_, vs) -> sort vs == [17, 61]) .
   M.toList . flip execState M.empty . runWriterT . runInstrs
 
 partB :: [Instr] -> Int
-partB instrs = product $ map snd $ filter ((`elem` [0, 1, 2]) . fst) outputs
+partB instrs =
+  product $ map snd $ filter ((`elem` [0, 1, 2]) . getOutputNo . fst) outputs
   where
-    outputs :: [(Int, Int)]
     outputs = flip evalState M.empty $ execWriterT (runInstrs instrs)
 
+day10 :: Day [Instr]
 day10 =
   Day
     10
