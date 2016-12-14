5691d928eec357da5dd51ec7ce830f0f92e94336
Steve Purcell <steve@sanityinc.com>
2016-12-14 22:03:47 +1300

    Rewrite Day 14 more neatly using list comprehensions

--- src/Day14.hs
+++ src/Day14.hs
@@ -6,9 +6,14 @@ import Day
 import qualified Crypto.Hash as H
 import Data.ByteString (ByteString)
 import Data.String (fromString)
-import Data.List (group)
-import Data.Maybe (listToMaybe, mapMaybe)
+import Data.List (group, tails)
 import Control.Arrow ((&&&))
+
+hexMD5 :: String -> String
+hexMD5 = show . md5 . fromString
+  where
+    md5 :: ByteString -> H.Digest H.MD5
+    md5 = H.hash
 
 hash :: String -> Int -> String
 hash salt n = hexMD5 (salt ++ show n)
@@ -17,46 +22,27 @@ stretchHash :: String -> Int -> String
 stretchHash salt n = iterate hexMD5 (salt ++ show n) !! 2017
 
 keys :: (Int -> String) -> [(Int, String)]
-keys = go . hashes
+keys hasher =
+  [ (n, h)
+  | ((n, h, triple, _), nexts) <- zip allTriples (tail (tails allTriples))
+  , any
+     (hasQuintuple triple)
+     (takeWhile (\(n', _, _, _) -> n' <= n + 1000) nexts) ]
   where
-    go ((Hash n hs c _):rest) =
-      if any (hasQuintuple c) (takeWhile ((<= (n + 1000)) . hIndex) rest)
-        then (n, hs) : go rest
-        else go rest
-    go _ = []
-
-hashes :: (Int -> String) -> [Hash]
-hashes hasher = mapMaybe (makeHashIfTriple hasher) [0 ..]
-
-data Hash = Hash
-  { hIndex :: !Int
-  , hHash :: !String
-  , hFirstTriple :: !Char
-  , hQuintuples :: !String
-  } deriving (Show)
-
-makeHashIfTriple :: (Int -> String) -> Int -> Maybe Hash
-makeHashIfTriple hasher n = do
-  triple <- firstTriple
-  return $ Hash n h triple quintuples
-  where
-    h = hasher n
-    groups = bigGroups h
-    firstTriple = fst <$> listToMaybe groups
-    quintuples = fst <$> filter ((>= 5) . snd) groups
-
-hasQuintuple c h = c `elem` hQuintuples h
+    allTriples :: [(Int, String, Char, String)]
+    allTriples =
+      [ (n, h, triple, quints)
+      | (n, h) <- (id &&& hasher) <$> [0 ..]
+      , let groups = bigGroups h
+      , not (null groups)
+      , let (triple, _) = head groups
+      , let quints = fst <$> filter ((>= 5) . snd) groups ]
+    hasQuintuple c (_, _, _, quints) = c `elem` quints
 
 bigGroups
   :: Eq a
   => [a] -> [(a, Int)]
 bigGroups = filter ((>= 3) . snd) . map (head &&& length) . group
-
-hexMD5 :: String -> String
-hexMD5 = show . md5 . fromString
-  where
-    md5 :: ByteString -> H.Digest H.MD5
-    md5 = H.hash
 
 partA = (!! 63) . keys . hash
 
