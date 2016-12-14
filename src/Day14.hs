8e9c6e09ceff29b2c45fd35e3245bc540d6aa575
Steve Purcell <steve@sanityinc.com>
2016-12-14 21:04:21 +1300

    Day 14b

--- src/Day14.hs
+++ src/Day14.hs
@@ -13,15 +13,20 @@ import Control.Arrow ((&&&))
 hash :: String -> Int -> String
 hash salt n = hexMD5 (salt ++ show n)
 
-keys :: String -> [(Int, String)]
+stretchHash :: String -> Int -> String
+stretchHash salt n = iterate hexMD5 (salt ++ show n) !! 2017
+
+keys :: (Int -> String) -> [(Int, String)]
 keys = go . hashes
   where
     go ((Hash n hs c _):rest) =
       if any (hasQuintuple c) (takeWhile ((<= (n + 1000)) . hIndex) rest)
         then (n, hs) : go rest
         else go rest
+    go _ = []
 
-hashes salt = mapMaybe (makeHashIfTriple salt) [0 ..]
+hashes :: (Int -> String) -> [Hash]
+hashes hasher = mapMaybe (makeHashIfTriple hasher) [0 ..]
 
 data Hash = Hash
   { hIndex :: !Int
@@ -30,12 +35,12 @@ data Hash = Hash
   , hQuintuples :: !String
   } deriving (Show)
 
-makeHashIfTriple :: String -> Int -> Maybe Hash
-makeHashIfTriple salt n = do
+makeHashIfTriple :: (Int -> String) -> Int -> Maybe Hash
+makeHashIfTriple hasher n = do
   triple <- firstTriple
   return $ Hash n h triple quintuples
   where
-    h = hash salt n
+    h = hasher n
     groups = bigGroups h
     firstTriple = fst <$> listToMaybe groups
     quintuples = fst <$> filter ((>= 5) . snd) groups
@@ -53,11 +58,13 @@ hexMD5 = show . md5 . fromString
     md5 :: ByteString -> H.Digest H.MD5
     md5 = H.hash
 
-partA = (!! 63) . keys
+partA = (!! 63) . keys . hash
+
+partB = (!! 63) . keys . stretchHash
 
 day14 =
   Day
     14
     (many1 letter <* newline)
     (return . show . partA)
-    (return . show . const "TODO")
+    (return . show . partB)
