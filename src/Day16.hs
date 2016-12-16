420a38489707f38719b2131526e4e57f77884539
Steve Purcell <steve@sanityinc.com>
2016-12-16 19:35:25 +1300

    Day 16: tidy up and add type signatures

--- src/Day16.hs
+++ src/Day16.hs
@@ -4,18 +4,21 @@ module Day16
 
 import Day
 
-expandData a = a ++ "0" ++ (dNot <$> (reverse a))
+expandData :: String -> String
+expandData a = a ++ "0" ++ (dNot <$> reverse a)
   where
     dNot '0' = '1'
     dNot '1' = '0'
     dNot _ = error "bad dNot arg"
 
+fill :: Int -> String -> String
 fill len input = checksum rawData
   where
     rawData = take len minExpansion
     minExpansion = head $ dropWhile ((<= len) . length) expansions
-    expansions = (iterate expandData input)
+    expansions = iterate expandData input
 
+checksum :: String -> String
 checksum xs = head $ dropWhile (even . length) $ iterate go xs
   where
     go ys@(a:b:_) =
@@ -29,5 +32,5 @@ day16 =
   Day
     16
     (many (oneOf "01") <* newline)
-    (return . show . fill 272)
-    (return . show . fill 35651584)
+    (return . fill 272)
+    (return . fill 35651584)
