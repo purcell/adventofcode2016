7e04cb6c319a4dcde1ea65d3154fc8fb912b7e52
Steve Purcell <steve@sanityinc.com>
2016-12-17 19:42:07 +1300

    Day 17b

--- src/Day17.hs
+++ src/Day17.hs
@@ -16,15 +16,15 @@ type Pos = (Int, Int)
 paths :: String -> (String, Pos) -> [(String, Pos)]
 paths seed start = go [start]
   where
-    go :: [(String, Pos)] -> [(String, Pos)]
     go [] = []
-    go (x@(path, pos):xs) = [x] ++ go xs ++ go nexts
+    go (x@(path, pos):xs) = x : go (xs ++ nexts)
       where
         hash = hexMD5 (seed ++ path)
         nexts =
           [ (path ++ [dir], pos')
           | (dir, c) <- zip "UDLR" hash
           , c `elem` "bcdef"
+          , pos /= (3, 3)
           , let pos'@(x', y') = move pos dir
           , x' >= 0
           , x' <= 3
@@ -37,9 +37,14 @@ move (x, y) 'L' = (x - 1, y)
 move (x, y) 'R' = (x + 1, y)
 move _ _ = error "bad move"
 
+solutions = fmap fst . filter (((3, 3) ==) . snd) . flip paths ("", (0, 0))
+
 partA :: String -> String
-partA = fst . head . dropWhile (((3, 3) /=) . snd) . flip paths ("", (0, 0))
+partA = head . solutions
+
+partB :: String -> Int
+partB = length . last . solutions
 
 main =
   runDay $
-  Day 17 (many letter <* newline) (return . partA) (return . const "TODO")
+  Day 17 (many letter <* newline) (return . partA) (return . show . partB)
