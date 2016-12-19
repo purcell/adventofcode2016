14d4b0fb5712e0d9c5e1b9dfdd6898417b6fa351
Steve Purcell <steve@sanityinc.com>
2016-12-19 22:13:51 +1300

    Day 19b

--- src/Day19.hs
+++ src/Day19.hs
@@ -5,12 +5,21 @@ import Data.Sequence (Seq)
 import Data.List (find)
 import qualified Data.Sequence as S
 
-elves :: Int -> Elf
-elves = (`S.index` 0) . runElves . makeElves
+elves :: (Seq Elf -> Seq Elf) -> Int -> Elf
+elves f n = (`S.index` 0) . f $ makeElves n
 
-runElves es
-  | S.length es >= 2 = runElves (S.drop 2 es S.>< S.take 1 es)
-runElves es = es
+runElvesA es
+  | S.length es >= 2 = runElvesA (S.drop 2 es S.>< S.take 1 es)
+runElvesA es = es
+
+runElvesB es
+  | S.length es >= 2 =
+    runElvesB (beforeSteal S.>< S.drop 1 fromSteal S.>< first)
+  where
+    (first, rest) = S.splitAt 1 es
+    (beforeSteal, fromSteal) = S.splitAt (nSteal - 1) rest
+    nSteal = S.length es `div` 2
+runElvesB es = es
 
 data Elf = Elf
   { eNum :: !Int
@@ -18,12 +27,10 @@ data Elf = Elf
 
 makeElves n = Elf <$> (S.iterateN n (+ 1) 1)
 
-partA = elves
-
 main =
   runDay $
   Day
     19
     ((read <$> many1 digit) <* newline)
-    (return . show . partA)
-    (return . show . const "TODO")
+    (return . show . elves runElvesA)
+    (return . show . elves runElvesB)
